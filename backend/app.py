c919bbbbfc90e18a8f30f7395b03c22dc9c41fe5
Prabhat625 <horseprabhat625@gmail.com>
2020-08-26 13:50:55 +0530

    table created

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app= Flask(__name__)
 db=SQLAlchemy(app)
 
-ENV='DEV'
-if ENV == 'DEV':
+ENV='PROD'
+if ENV == 'PROD':
     app.config['SQLALCHEMY_DATABASE_URI']='postgres://xkwjdpddhokzfg:8a678a487d63f4e0944d44177025f754d112ba7e305b0ef442356836df0fff0d@ec2-52-204-20-42.compute-1.amazonaws.com:5432/d4b9i9tq38h5b4'
 else :
    app.config.update({'SQLALCHEMY_DATABASE_URI':'postgres://postgres:123456@localhost/todo_db'})
